f9ad8a6dc55b0f754edc9de2f879d0088344bea7
Michael Greenly <mgreenly@gmail.com>
2025-06-19 18:19:17 -0500

    experimenting with queue

--- src/client.c
+++ src/client.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 #include "log.h"
@@ -9,13 +10,27 @@
 #define PORT 4242
 #define HOST "127.0.0.1"
 
+#define QUEUE_CAPACITY 256
+
 //
 // main
 //
 int32_t main(void) {
-  log_info("%s", "doing stuff in main");
+  queue_t* msg_queue = queue_create(QUEUE_CAPACITY);
+  if (!msg_queue) {
+    log_error("%s", "Failed to create message queue. Exiting.");
+    return EXIT_FAILURE;
+  }
 
-  puts("Hello from the client!");
+  char *greeting = "Hello from the client!";
+
+  queue_push(msg_queue, greeting);
+
+  puts(queue_pop(msg_queue));
+
+  queue_destroy(msg_queue);
+
+
   return EXIT_SUCCESS;
 }
 
--- src/queue.h
+++ src/queue.h
@@ -7,7 +7,8 @@
 #include "message.h"
 
 typedef struct {
-    message_t** buffer;
+    // message_t** buffer;
+    char **buffer;
     size_t capacity;
     size_t size;
     size_t head;
