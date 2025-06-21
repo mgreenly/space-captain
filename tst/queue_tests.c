b4357a8fc7337d081fca70a7755f6a677eb26526
Michael Greenly <mgreenly@gmail.com>
2025-06-20 19:52:54 -0500

    working queue test

--- tst/queue_tests.c
+++ tst/queue_tests.c
@@ -1,10 +1,33 @@
+#include <string.h>
+
 #include "vendor/unity.c"
 
+#include "../src/message.h"
 #include "../src/queue.h"
 #include "../src/queue.c"
 
-void test_queue_foo(void) {
-  TEST_ASSERT_EQUAL(0, 0);
+void test_queue_add_and_pop_message(void) {
+  queue_t *queue = queue_create(10);
+  TEST_ASSERT_NOT_NULL(queue);
+
+  char *test_message = "This is the test message";
+
+  message_t msg = {
+    .header = {
+      .type = MSG_ECHO,
+      .length = strlen(test_message)
+    },
+    .body = test_message
+  };
+
+  queue_push(queue, &msg);
+
+  message_t *popped_msg = queue_pop(queue);
+  TEST_ASSERT_NOT_NULL(popped_msg);
+  TEST_ASSERT_EQUAL(MSG_ECHO, popped_msg->header.type);
+  TEST_ASSERT_EQUAL_STRING(test_message, popped_msg->body);
+
+  queue_destroy(queue);
 }
 
 void setUp(void) { }
@@ -15,6 +38,6 @@ int main(void)
 {
   puts("==============================================");
   UnityBegin("tst/queue_tests.c");
-  RUN_TEST(test_queue_foo);
+  RUN_TEST(test_queue_add_and_pop_message);
   return (UnityEnd());
 }
