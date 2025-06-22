96fe5fbf87c493fd43ee0b190df6933d722789be
Michael Greenly <mgreenly@gmail.com>
2025-06-22 07:57:25 -0500

    remove extra whitespace

--- tst/queue_tests.c
+++ tst/queue_tests.c
@@ -42,56 +42,56 @@ typedef struct {
 
 void* producer_thread(void* arg) {
   thread_data_t *data = (thread_data_t*)arg;
-  
+
   usleep(data->delay_ms * 1000);
-  
+
   char buffer[64];
   snprintf(buffer, sizeof(buffer), "Test message %d", data->test_value);
-  
+
   message_t *msg = malloc(sizeof(message_t));
   msg->header.type = MSG_ECHO;
   msg->header.length = strlen(buffer);
   msg->body = strdup(buffer);
-  
+
   queue_add(data->queue, msg);
-  
+
   return NULL;
 }
 
 void* consumer_thread(void* arg) {
   thread_data_t *data = (thread_data_t*)arg;
-  
+
   message_t *msg = queue_pop(data->queue);
-  
+
   data->test_value = (msg != NULL) ? 1 : 0;
-  
+
   if (msg) {
     free(msg->body);
     free(msg);
   }
-  
+
   return NULL;
 }
 
 void test_queue_pop_blocks_until_push(void) {
   queue_t *queue = queue_create(5);
   TEST_ASSERT_NOT_NULL(queue);
-  
+
   pthread_t producer, consumer;
   thread_data_t producer_data = {queue, 100, 42};
   thread_data_t consumer_data = {queue, 0, 0};
-  
+
   pthread_create(&consumer, NULL, consumer_thread, &consumer_data);
-  
+
   usleep(3000);
-  
+
   pthread_create(&producer, NULL, producer_thread, &producer_data);
-  
+
   pthread_join(consumer, NULL);
   pthread_join(producer, NULL);
-  
+
   TEST_ASSERT_EQUAL(1, consumer_data.test_value);
-  
+
   queue_destroy(queue);
 }
 
